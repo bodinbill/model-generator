897a21c2869b7b730957488a9ab746272001521b
rudw <rud@wongnai.com>
2016-02-29 12:47:40 +0700

    add execute method

--- lib/template/repositoryhibernatetest.py
+++ lib/template/repositoryhibernatetest.py
@@ -2,4 +2,7 @@ from template import Temlpate, TemplateUtils
 
 class RepositoryHibernateTemplate(Template):
     def __init__(self, datas):
+        super(self.__class__, self).__init__(datas);
+
+    def execute(self):
         pass
